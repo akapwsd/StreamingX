ba000b37e6d97264c6a0783fbd681bb231f51a98
unknown <765657797@qq.com>
2023-03-27 14:16:49 +0800

    some new code

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,54 +9,17 @@ import android.os.IBinder;
 import android.util.Log;
 import android.widget.Button;
 
+import com.example.rtc.WebSocketManager;
 import com.example.sokect.JWebSocketClient;
 import com.example.sokect.WebSocketService;
 
 public class MainActivity extends Activity {
-
-    private JWebSocketClient client;
-    private WebSocketService.JWebSocketClientBinder binder;
-    private WebSocketService jWebSClientService;
-    private ServiceConnection serviceConnection = new ServiceConnection() {
-        @Override
-        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
-            //服务与活动成功绑定
-            Log.d("ZHIZHI", "service bind success");
-            binder = (WebSocketService.JWebSocketClientBinder) iBinder;
-            jWebSClientService = binder.getService();
-            client = jWebSClientService.client;
-        }
-
-        @Override
-        public void onServiceDisconnected(ComponentName componentName) {
-            Log.d("ZHIZHI", "service disconnect");
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bindService();
+        WebSocketManager.getInstance().bindService(this);
         Button sendBtn = findViewById(R.id.send_btn);
-        sendBtn.setOnClickListener(view -> sendMsg("123"));
+        sendBtn.setOnClickListener(view -> WebSocketManager.getInstance().sendMsg("123"));
     }
-
-    private void bindService() {
-        Log.d("ZHIZHI", "bindService is start");
-        Intent bindIntent = new Intent(MainActivity.this, WebSocketService.class);
-        bindService(bindIntent, serviceConnection, BIND_AUTO_CREATE);
-    }
-
-    public void sendMsg(String msg) {
-        if (null != client) {
-            Log.i("ZHIZHI", "send msg：" + msg);
-            try {
-                client.send(msg);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
 }
