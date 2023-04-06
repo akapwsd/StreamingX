34b9cf5c93af3755dc9ed787f27739c9865fb172
unknown <765657797@qq.com>
2023-04-06 11:30:28 +0800

    add new code

--- youyu/src/main/java/com/example/youyu/api/RtcManager.java
+++ youyu/src/main/java/com/example/youyu/api/RtcManager.java
@@ -2,8 +2,14 @@ package com.example.youyu.api;
 
 
 import android.content.Context;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.FrameLayout;
 
 import com.example.rtc.BaseRtcEngineManager;
+
+import io.agora.rtc.RtcEngine;
+import io.agora.rtc.video.VideoCanvas;
 
 public class RtcManager {
     private static final class RtcManagerHolder {
@@ -14,7 +20,30 @@ public class RtcManager {
         return RtcManagerHolder.rtcManager;
     }
 
-    public void initRtc(Context context){
+    public void initRtc(Context context) {
         BaseRtcEngineManager.getInstance().initBaseRtc(context);
+    }
+
+    public void showRemoteView(Context context, int uid, FrameLayout view) {
+        SurfaceView surfaceView = RtcEngine.CreateRendererView(context);
+        RtcEngine rtcEngine = BaseRtcEngineManager.getInstance().getRtcEngine();
+        rtcEngine.setupRemoteVideo(new VideoCanvas(surfaceView, VideoCanvas.RENDER_MODE_HIDDEN, uid));
+        view.addView(surfaceView, 0, new FrameLayout.LayoutParams(FrameLayout.LayoutParams.MATCH_PARENT,
+                FrameLayout.LayoutParams.MATCH_PARENT));
+    }
+
+    public void showLocalView(Context context, int uid, FrameLayout view) {
+        RtcEngine rtcEngine = BaseRtcEngineManager.getInstance().getRtcEngine();
+        SurfaceView surfaceView = RtcEngine.CreateRendererView(context);
+        surfaceView.setZOrderOnTop(true);
+        view.addView(surfaceView);
+        rtcEngine.setupLocalVideo(
+                new VideoCanvas(
+                        surfaceView,
+                        VideoCanvas.RENDER_MODE_HIDDEN,
+                        uid
+                )
+        );
+        surfaceView.setZOrderMediaOverlay(true);
     }
 }
