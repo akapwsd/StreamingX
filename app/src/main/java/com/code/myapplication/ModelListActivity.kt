e03a403e7749398422e864be6fc834ed92652fd8
unknown <765657797@qq.com>
2023-05-19 15:19:17 +0800

    change code

--- app/src/main/java/com/code/myapplication/ModelListActivity.kt
+++ app/src/main/java/com/code/myapplication/ModelListActivity.kt
@@ -1,20 +1,24 @@
 package com.code.myapplication
 
 import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.code.bean.ModelBean
 import com.code.bean.ModelListBean
+import com.code.listener.HttpRequestListener
 import com.code.listener.RequestModelListListener
 import com.code.utils.HttpRequestUtils
+import com.code.youyu.api.RtcManager
 import kotlinx.android.synthetic.main.activity_model_list.*
 
 class ModelListActivity : Activity() {
     private var list: ArrayList<ModelBean>? = arrayListOf()
     private var adapter: ModelAdapter? = null
-
+    private val channelId = "2825806909305530098"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_model_list)
@@ -27,6 +31,15 @@ class ModelListActivity : Activity() {
         adapter?.setModelItemClickListener(object : ModelAdapter.ModelItemClickListener {
             override fun onItemClickListener(model: ModelBean) {
                 val id = model.id
+                RtcManager.getInstance().callVideo(channelId, "110", object : HttpRequestListener {
+                    override fun requestSuccess(o: Any?) {
+                        startActivity(Intent(this@ModelListActivity, AgoVideoActivity::class.java))
+                    }
+
+                    override fun requestError(code: Int, error: String?) {
+                        Toast.makeText(this@ModelListActivity, error, Toast.LENGTH_LONG).show()
+                    }
+                })
             }
         })
 
